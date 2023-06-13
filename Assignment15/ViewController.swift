6af03d2e8fd7b199edb5134dc07ab8ba80519a9a
Foxxyy <mikethesupertramp@gmail.com>
2023-06-13 21:11:41 +0400

    Initial Commit

---
+++ Assignment15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment15
+//
+//  Created by macbook  on 13.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
